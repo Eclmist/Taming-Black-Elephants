f2afba779b01023f1f2092eaa36007ca09931051
Samuel Ng <eclmist@yahoo.com>
2017-12-26 12:35:33 +0800

    Added scene loader

---
+++ Assets/Scripts/Interactables/LevelTransition.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class LevelTransition : MonoBehaviour {
+
+    public string targetScene = "";
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            LoadScene();
+        }
+    }
+
+    private void LoadScene()
+    {
+
+    }
+
+
+}
