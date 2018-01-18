1875b2f698c2d5016c1e8c5c269fc035ecb2d9aa
Samuel Ng <eclmist@yahoo.com>
2018-01-18 15:02:31 +0800

    Added troll door
    Fixed carrot location
    Fixed Rogue paper in room

---
+++ Assets/Scripts/Misc/CyroTrollDoor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CyroTrollDoor : MonoBehaviour 
+{
+
+    Animator anim;
+
+	protected void Start () 
+	{
+        anim = GetComponent<Animator>();
+        if (!anim)
+            Destroy(this);
+	}
+	
+	protected void Update () 
+	{
+		
+	}
+
+    protected void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            if (anim)
+            {
+                anim.SetTrigger("Close");
+            }
+        }
+    }
+}
